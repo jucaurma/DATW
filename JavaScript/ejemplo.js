58a7413d334fd7f0504ab13164f2df8820e1bfe9
jucaurma <155864978+jucaurma@users.noreply.github.com>
2024-04-23 11:21:01 +0200

    first commit

---
+++ JavaScript/ejemplo.js
@@ -0,0 +1,10 @@
+function isEven(number) {
+    return number % 2 === 0;
+}
+// Example usage:
+var myNumber = 9;
+if (isEven(myNumber)) {
+    console.log(myNumber + " is even.");
+} else {
+    console.log(myNumber + " is odd.");
+}
