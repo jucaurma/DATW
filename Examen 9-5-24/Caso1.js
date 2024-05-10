cfa57ab75a57a21696132793de22663b4d642dda
jucaurma <155864978+jucaurma@users.noreply.github.com>
2024-05-10 15:28:23 +0200

    10 de mayo

--- Examen 9-5-24/Caso1.js
+++ Examen 9-5-24/Caso1.js
@@ -1,11 +1,11 @@
 
-// escribe un programa en javascript que determine si el número indicado en una constante es par o impar, mostrando el resultado en consola
+// Escribe un programa en javascript que determine si el número indicado en una constante es par o impar, mostrando el resultado en consola.
 
 
-// Definir una constante con el número a verificar
+// Definimos una constante con el número a verificar
 const numero = 9;  
 
-// Función para determinar si el número es par o impar
+// En la función determinamos que el número es par cuando al ser dividido entre 2 el resto es 0. En caso contrario es impar.
 function verificarParidad(num) {
     if (num % 2 === 0) {
         console.log(`El número ${num} es par.`);
